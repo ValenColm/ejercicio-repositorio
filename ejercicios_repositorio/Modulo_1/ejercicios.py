1edb3ce13079b558206f18368e873e83991b4632
Valentina <dismarybolivar1711@gmail.com>
2025-11-19 13:55:40 -0500

    Fifteen_Commit

---
+++ ejercicios_repositorio/Modulo_1/ejercicios.py
@@ -0,0 +1,32 @@
+
+
+#ejercicio 1
+with open("frutas.txt", "w") as file:
+        for i in range (1,6):       
+            fruits =  input("ingresa una fruta : ")
+            file.write(fruits+  "\n")
+            
+#ejercicio 2
+with open ("frutas.txt", "r") as file:
+        lineas = file.readlines()
+        print(f"cantidad de lineas:", len(lineas))
+        
+#ejercicio 3
+palabra = ("ingresa una palabra a buscar").lower()
+
+with open ("frutas.txt", "r") as file:
+    for linea in file:
+        if palabra in linea.lower():
+            print("encontrado en :", {linea})
+            
+#ejercicio 4
+while True:
+    tarea = input("Escribe una tarea (o 'salir'): ")
+    if tarea.lower() == "salir":
+        break
+    with open("tareas.txt", "a") as file:
+        file.write(tarea + "\n")
+
+# Mostrar todas
+with open("tareas.txt", "r") as file:
+    print(file.read())
